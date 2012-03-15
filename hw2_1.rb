019b6bcc99fa40e3f0898b9da33c1eef9f799f10
Dmytro Osadchyy <tselina@gmail.com>
2012-03-16 00:01:48 +0200

    Add method to calculate conversion

--- hw2_1.rb
+++ hw2_1.rb
@@ -1,5 +1,5 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'dollar' => 1, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -8,11 +8,23 @@ class Numeric
       super
     end
   end
+  
+  def in(currency)
+	singular_currency = currency.to_s.gsub( /s$/, '')
+
+    if @@currencies.has_key?(singular_currency)
+      self / @@currencies[singular_currency]
+    else
+      super
+    end
+	
+  end
 end
 
-5.dollars.in(:euros)  # 5 / 1.292
-10.euros.in(:rupees)  #
-1.dollar.in(:rupees)
-1.dollar.in(:rupee)
-10.rupees.in(:euro)
-10.rupee.in(:euro)    
+#20.euros
+puts 5.dollars.in(:euros)  # 5 / 1.292
+# puts 10.euros.in(:rupees)  #
+# puts 1.dollar.in(:rupees)
+# puts 1.dollar.in(:rupee)
+# puts 10.rupees.in(:euro)
+# puts 10.rupee.in(:euro)    
